fc492591be4e3c2f30a1988edf9334125c86fd09
Darren Wang <darrenwng6@gmail.com>
2022-09-15 23:52:25 -0700

    style token page + add commenting functionality

--- src/pages/Token.tsx
+++ src/pages/Token.tsx
@@ -1,18 +1,41 @@
-import { Box, Image, Icon } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  Icon,
+  Button,
+  Stack,
+  Spacer,
+  Heading,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Textarea,
+  Avatar,
+} from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 import { useEffect, useContext, useState } from "react";
-
 import { AppContext } from "../context";
 import { Container } from "../components/Container";
 
 const Token = (props: any) => {
-  const { hyperClient } = useContext(AppContext);
+  const { hyperClient, likes, setLikes, collections, setCollections } =
+    useContext(AppContext);
 
   // TODO: set this in Context?
   let address: string;
 
   const [tokenInfo, setTokenInfo] = useState({});
+  const [showCollectionModal, setShowCollectionModal] = useState(false);
+  const [value, setValue] = useState("");
+  const [comments, setComments] = useState([]);
+
+  const isLiked: boolean = likes.some(
+    (like) => like.token_address === tokenInfo.token_address
+  );
 
   useEffect(() => {
     address = window.location.href.split("/")[4];
@@ -34,44 +57,124 @@ const Token = (props: any) => {
 
   const handleLike = () => {
     // add to my likes: all token info
+    if (isLiked) return;
+    setLikes([...likes, tokenInfo]);
   };
 
   const handleAdd = () => {
-    // add to collection: all token info
+    // show modal to add to collection
+    setShowCollectionModal(true);
+
+    let newCollection: any = {};
+    // add to existing collection: all token info
+    // if () {
+    //     newCollection = {
+    //         name: ,
+    //         tokens: [...tokens, tokenInfo]
+    //     };
+    // };
+
+    // // create a new collection
+    // else {
+    //     newCollection = {
+    //         name: ,
+    //         tokens: [tokenInfo]
+    //     };
+    // };
+
+    // setCollections([...collections, newCollection]);
+  };
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handlePost = () => {
+    setComments([...comments, value]);
+    setValue("");
   };
 
   return (
     <Container>
-      <Box>
+      <Stack spacing={4} w="500px" align="justify-content">
         <Image
           src={tokenInfo.meta_data_img}
           alt={tokenInfo.name}
-          style={{ width: "500px", height: "auto" }}
+          style={{ width: "400px", height: "auto", margin: "auto" }}
         />
-        <h3>{tokenInfo.name}</h3>
+        <Stack direction="row" spacing={4} align="center">
+          <Heading as="h3" size="lg">
+            {tokenInfo.name}
+          </Heading>
+          <Spacer />
+          <button onClick={() => handleLike()}>
+            {isLiked ? (
+              <Icon as={FaHeart} style={{ color: "red" }} />
+            ) : (
+              <Icon as={FaRegHeart} />
+            )}
+          </button>
+          <Button onClick={() => handleAdd()}>
+            <AddIcon />
+          </Button>
+        </Stack>
 
-        <button onClick={() => handleLike()}>
-          <Icon as={FaRegHeart} />
-        </button>
-        <button onClick={() => handleAdd()}>
-          <AddIcon />
-        </button>
-
-        <p>
+        <Text fontSize="lg">
           Price:{" "}
           {tokenInfo.market_place_state && tokenInfo.market_place_state.price}{" "}
           SOL
-        </p>
+        </Text>
 
-        <button>Buy Now</button>
-        <button>Make Offer</button>
-      </Box>
+        <Stack direction="row" spacing={4} align="center">
+          <Button colorScheme="blue">Buy Now</Button>
+          <Button colorScheme="gray">Make Offer</Button>
+        </Stack>
 
-      <Box>
-        <p>Attributes</p>
-        <p>Details</p>
-        <p>Comments</p>
-      </Box>
+        <Tabs>
+          <TabList>
+            <Tab>Attributes</Tab>
+            <Tab>Details</Tab>
+            <Tab>Comments</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <Text fontSize="md">
+                <pre>{JSON.stringify(tokenInfo.attributes, null, 4)}</pre>
+              </Text>
+            </TabPanel>
+            <TabPanel>
+              <Text fontSize="md">
+                <pre style={{ width: "500px", overflow: "hidden" }}>
+                  {JSON.stringify(tokenInfo.market_place_state, null, 2)}
+                </pre>
+              </Text>
+            </TabPanel>
+            <TabPanel>
+              <Stack spacing={4}>
+                {comments.map((comment) => (
+                  <Stack direction="row" spacing={4} align="center">
+                    <Avatar
+                      name="Default User"
+                      src="https://bit.ly/dan-abramov"
+                    />
+                    <Text fontSize="md">User: </Text>
+                    <Text fontSize="md">{comment}</Text>
+                  </Stack>
+                ))}
+                <Textarea
+                  placeholder="Add a comment"
+                  value={value}
+                  onChange={handleInputChange}
+                />
+                <Button colorScheme="blue" onClick={() => handlePost()}>
+                  Post
+                </Button>
+              </Stack>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Stack>
     </Container>
   );
 };
